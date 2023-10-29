e5101cb3576d3ec4d4abffec0084bb6a62c13d3e
Neeraj Pandey <neeraj.pandey01@ad.infosys.com>
2023-10-29 11:41:31 +0530

    Revert "Commited in local repo"
    This reverts commit 4543501ba94c803a4fe19657f3662bd73d15f1cc.
    
    revert the changes

--- src/main/java/com/ni/repo/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ni.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ni.model.Employee;
-
-public interface EmployeeRepo extends JpaRepository<Employee, Integer>{
-
-}
